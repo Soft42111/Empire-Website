32065dc9f06d10045ca72325c4a5e5ba5f54b00f
soft42111 <soft4211@email.com>
2025-07-15 20:00:23 +0500

    Fixed Watermark image

--- src/components/Watermark.tsx
+++ src/components/Watermark.tsx
@@ -10,7 +10,7 @@ const Watermark = () => {
       target="_blank"
       rel="noopener noreferrer"
     >
-      <img src="/public/WhatsApp Image 2025-07-13 at 17.15.05_130c299c.jpg" alt="Saad" className="saad-image" />
+      <img src=".\/WhatsApp Image 2025-07-13 at 17.15.05_130c299c.jpg" alt="Saad" className="saad-image" />
       <strong>Made by Saad Hassan</strong>
     </a>
   );
